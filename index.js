8400c303ca3d430535589b20e0c9f50a27e4bf87
ayush <ayushjaiswal2902@gmail.com>
2025-05-12 21:37:19 +0530

    added new function featc

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const age = 21;
 const name1 = 'ayush';
-
+const feat = "hi"
 function abc(){
     return 1 + 2 + 3
 }
 function newfeat(){
     return name1
+}
+
+function featc(){
+    return feat 
 }
